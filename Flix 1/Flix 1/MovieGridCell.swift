0eee4df16981175a330b0d6ba731a66c8455279f
Zeizul <connergleaves@gmail.com>
2022-10-10 23:57:02 -0400

    Added Flix 2 to repository

---
+++ Flix 1/Flix 1/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix 1
+//
+//  Created by Conner Gleaves on 10/10/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
